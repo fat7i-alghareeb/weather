5b593cadb3b2a1237a78ff6ce961f42f1046de2d
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-10-14 11:14:18 +0300

    add required fonts and images

---
+++ lib/utils/app_text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle w700s64 = const TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 64,
+  );
+
+  static TextStyle w400s24 = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 24,
+  );
+
+  static TextStyle w600s20 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static TextStyle w500s20 = const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+
+  static TextStyle w500s64 = const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 64,
+  );
+
+  static TextStyle w700s24 = const TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+  );
+
+  static TextStyle w600s28 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 28,
+  );
+
+  static TextStyle w600s18 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+
+  static TextStyle w400s16 = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+}
---
+++ lib/utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  Assets._();
+
+  static const String onBoarding = "images/onBoarding.png";
+  static const String morning = "images/morning.png";
+  static const String night = "images/night.png";
+  static const String house = "images/house.png";
+  static const String sun = "images/sun.png";
+  static const String air = "images/air.png";
+  static const String arrowLeft = "images/arrowLeft.png";
+  static const String arrowRight = "images/arrowRight.png";
+}
