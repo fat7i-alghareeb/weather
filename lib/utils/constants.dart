a987599ada7b66341d1d9f824dc47a1a3dffabb1
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-10-14 16:56:02 +0300

    create service class

---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const apiKey = "ff8aac2c1d088bd9e96d464e4c6c1268";
+}
---
+++ lib/utils/services/api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import '../../features/home/data/models/weather_model.dart';
+import '../constants.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: "http://api.weatherstack.com/")
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+  @GET("/forecast")
+  Future<WeatherModel> getWeather(
+      {@Query("access_key") String apiKey = Constants.apiKey,
+      @Query("hourly") int hourly = 1,
+      @Query("query") String town = "Aleppo"});
+}
