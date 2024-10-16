80d8509f985a585b77f87b19af87d92a9b4c4f89
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-10-16 07:14:45 +0300

    finishes from adding the new api and integrate them into the app ui

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,7 +4,7 @@ import 'package:responsive_framework/responsive_framework.dart';
 
 class Constants {
   static const apiKey = "ff8aac2c1d088bd9e96d464e4c6c1268";
-  static double borderRadius = 50.0.r;
+  static final double borderRadius = 50.0.r;
   static ResponsiveValue<double> responsiveValue(BuildContext context) =>
       ResponsiveValue<double>(context, conditionalValues: [
         const Condition.between(start: 0, end: 400, value: 430),
@@ -13,14 +13,5 @@ class Constants {
         const Condition.between(start: 1000, end: 1400, value: 500),
         const Condition.between(start: 1400, end: 9999, value: 900),
       ]);
-
-  static List<String> days = [
-    "Sat",
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-  ];
+  static final DateTime today = DateTime.now();
 }
